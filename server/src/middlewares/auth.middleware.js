f9f8ed550e68474959fcd41ff85da83adeb7fd87
Anshul Sharma <sharma.ansh2926@gmail.com>
2025-03-21 11:47:52 +0530

    Add authentication middleware with JWT verification and role checks

---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,63 @@
+import { Student } from "../models/students.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "").trim();
+
+        if (!token) {
+            throw new ApiError(403, "Unauthenticated request");
+        }
+
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        const user = await User.findById(decodedToken?._id);
+
+        if (!user) {
+            throw new ApiError(403, "Invalid Access Token");
+        }
+
+        req.user = user;
+        next();
+    } catch (err) {
+        console.error(`Error in user authentication: ${err}`);
+        throw new ApiError(403, err?.message || "Invalid Access Token");
+    }
+});
+
+
+export const verifyAdmin = asyncHandler(async(req, res, next) => {
+    try{
+        const { user } = req;
+        if(!user || user.role !== 'ADMIN'){
+            throw new ApiError(403, "Access forbidden");
+        }
+        next();
+
+    }catch(err){
+        throw new ApiError(403, err?.message || "Un-authorized access denied !!");
+    }
+})
+export const verifyEmployee = asyncHandler(async(req, res, next) => {
+    try{
+        const { user } = req;
+        if(!user || user.role !== 'Employee'){
+            throw new ApiError(403, "Access forbidden");
+        }
+        next();
+
+    }catch(err){
+        throw new ApiError(403, err?.message || "Un-authorized access denied !!");
+    }
+})
+
+
+// to check if the user is student,we will only use verifyJWT and if we want to check if the user is employee or admin, then we will also check verifyAdmin / verifyEmployee along with the verifyJWT
+
+
+
+
+
+
