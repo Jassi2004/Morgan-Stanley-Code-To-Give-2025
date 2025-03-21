97be13dd37dcd2a919e840e526841585cb92fea1
Anshul Sharma <sharma.ansh2926@gmail.com>
2025-03-21 21:38:42 +0530

    Refactor employee routes and add student authentication routes

--- server/src/index.js
+++ server/src/index.js
@@ -2,10 +2,8 @@ import { app } from "./app.js";
 import connectDB from "./db/connectDb.js";
 import dotenv from "dotenv";
 
-dotenv.config({ path : "./.env" });
+dotenv.config({ path: "./.env" });
 const PORT = process.env.PORT || 5000;
-
-
 
 connectDB()
     .then(() => {
