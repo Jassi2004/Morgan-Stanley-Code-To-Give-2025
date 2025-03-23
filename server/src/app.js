a1f11b22eeea5a07a42919a96a8d77f549fd2f2e
Jassi2004 <bedijaskirat2004@gmail.com>
2025-03-23 10:57:35 +0530

    some more things

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,10 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin :[process.env.FRONTEND_URL],
+    credentials : true
+}))
 
 
 app.use(express.json({ limit : "10mb" }));
@@ -20,7 +23,7 @@ import studentRouter from "./routes/student.routes.js";
 import notificationRouter from "./routes/notification.routes.js"
 
 
-app.use("/api/v1/employee", employeeRouter);
+app.use("/api/v1/employee", employeeRouter); 
 app.use("/api/v1/student", studentRouter);
 app.use("/api/v1/notification", notificationRouter);
 
